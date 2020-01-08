1eb43e4021392668d491df04c60968b0ac9f68bd
JaiMartz <martinezgarridojairo@gmail.com>
2020-01-08 03:40:40 +0100

    First Commit
    Simple form application with SWING

---
+++ Tarea01_DI/src/tarea01_di/Tarea01_DI.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea01_di;
+
+/**
+ *
+ * @author Jairo Martínez Garrido
+ */
+public class Tarea01_DI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Interfaz v = new Interfaz();
+        v.setVisible(true);
+    }
+    
+}
